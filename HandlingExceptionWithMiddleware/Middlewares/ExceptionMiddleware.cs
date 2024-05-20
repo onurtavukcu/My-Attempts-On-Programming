8ba36558929147f98a8264759a404529c466d378
onurtavukcu <onur.tavukcu@xyzteknoloji.com>
2024-05-20 17:11:33 +0300

    Exception Middleware Added

---
+++ HandlingExceptionWithMiddleware/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace HandlingExceptionWithMiddleware.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await httpContext
+                    .Response
+                    .WriteAsJsonAsync("Test exception Middleware. Here is the response body", cancellationToken: CancellationToken.None);
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
